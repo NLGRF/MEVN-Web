e7c133797516a7c3e8cada5f4c3071258558d6f0
NLGRF <nongrace6654@gmail.com>
2018-03-29 22:34:24 +0700

    Finish Page

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Posts from '@/components/Posts'
+import NewPost from '@/components/NewPost'
 
 Vue.use(Router)
 
@@ -9,13 +10,18 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
+      name: 'Hello',
       component: HelloWorld
     },
     {
       path: '/posts',
       name: 'Posts',
       component: Posts
+    },
+    {
+      path: '/posts/new',
+      name: 'NewPost',
+      component: NewPost
     }
   ]
 })
